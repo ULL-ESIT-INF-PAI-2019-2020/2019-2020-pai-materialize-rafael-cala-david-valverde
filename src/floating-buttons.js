f27dd672fc5089f99155f59bc20ec07c0f98b894
David Valverde <alu0101100296@ull.edu.es>
2020-05-08 20:16:12 +0100

    Más ejemplos

--- src/floating-buttons.js
+++ src/floating-buttons.js
@@ -1,6 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.fixed-action-btn');
-  var instances = M.FloatingActionButton.init(elems, {
-    direction: 'top'
+
+  document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+      direction: 'top'
+    });
   });
-});
