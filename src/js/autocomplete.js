b85b5a33c28a2b0af359f4ed25311d39607a997a
David Valverde <alu0101100296@ull.edu.es>
2020-05-09 22:34:14 +0100

    Ejemplos terminados

---
+++ src/js/autocomplete.js
@@ -0,0 +1,17 @@
+var options = {
+  data: {
+    "Java": "../img/logo-java.png",
+    "Javascript": "../img/logo-js.png",
+    "CSS": "../img/logo-css.png",
+    "HTML": "../img/logo-html.png",
+    "NodeJS": "../img/logo-nodejs.png",
+    "PHP": "../img/logo-php.png",
+    "Python": "../img/logo-python.png",
+    "C++": "../img/logo-cpp.png",
+    "C#": "../img/logo-csharp.png" 
+  },
+}
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.autocomplete');
+  var instances = M.Autocomplete.init(elems, options);
+});
